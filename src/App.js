f2c72476b3fbad33e599c463461ddc0d0dbee19a
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-03-25 13:43:14 +0300

    getData function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import axios from "axios";
+import Posts from "./components/Posts";
+
+export const getData = async (userId) => {
+  const [userData, postData] = await Promise.all([
+    axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`),
+    axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`),
+  ]);
+  return { user: userData.data, posts: postData.data };
+};
+
 function App() {
   return (
-    <div className="App">
-      <span>ilk odev</span>
+    <div>
+      <Posts />
     </div>
   );
 }
