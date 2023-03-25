fad1c791f0d7577fc1b8e05e33c7f9bf89ff914c
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-03-25 13:54:19 +0300

    açıklama ekledim

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,6 +6,7 @@ function Posts() {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
+    //number yoksa bütün verileri ekrana yazdırmasın diye if kontrolü açtım
     if (number !== "") {
       const fetchData = async () => {
         try {
@@ -22,8 +23,8 @@ function Posts() {
     <div>
       <div className="grid justify-items-center">
         <input
-          className="border mt-10"
-          placeholder="Bir sayı giriniz."
+          className="border rounded-md mt-10 p-2 placeholder:text-black focus:placeholder:text-gray-300"
+          placeholder="Bir id giriniz."
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
